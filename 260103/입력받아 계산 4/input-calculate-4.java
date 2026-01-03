cb429258e78398251061c6efef9a5ec32549590b
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2026-01-03 14:22:56 +0900

    Title: 입력받아 계산 4, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260103/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        System.out.printf("%d", a*2);
+    }
+}
