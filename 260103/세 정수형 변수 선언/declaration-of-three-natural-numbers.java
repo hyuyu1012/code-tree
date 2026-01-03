5920c7d51ac8db74da77c92e3c15bc6b2de65dcd
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2026-01-03 13:38:32 +0900

    Title: 세 정수형 변수 선언, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

--- 260103/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
+++ 260103/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -4,7 +4,7 @@ public class Main {
         a = 7;
         b = 23;
         c = 30;
-        //System.out.printf("%d + %d = %d", a,b,c);
-        System.out.printf(a,b,c);
+        System.out.printf("%d + %d = %d", a,b,c);
+
         }
 }
