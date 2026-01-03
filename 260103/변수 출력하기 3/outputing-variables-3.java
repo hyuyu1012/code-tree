6f114c8299aef07af3fbd6d8eed5b9c3ecf681c4
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2026-01-03 13:40:15 +0900

    Title: 변수 출력하기 3, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260103/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 1;
+        b = 2;
+        c = 'C';
+        System.out.printf("%d->%d->%c", a,b,c);
+    }
+}
