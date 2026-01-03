71376d74f451a6e6a9a682ed49b6795f0f241943
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2026-01-03 14:22:05 +0900

    Title: 점수 입출력, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260103/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int N;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        System.out.printf("Your score is %d point.", N);
+    }
+}
