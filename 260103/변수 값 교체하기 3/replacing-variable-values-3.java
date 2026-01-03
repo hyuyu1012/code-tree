7635a2d9b7e14f712383c9e0fb32d7ce795ed54f
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2026-01-03 14:10:47 +0900

    Title: 변수 값 교체하기 3, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260103/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a,b,temp;
+        a = 3;
+        b = 5;
+        temp = a;
+        a = b;
+        b = temp;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
