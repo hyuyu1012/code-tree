c067500c7a8f1bd18dfc1069cdcca94cce70aa6a
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2026-01-03 15:04:58 +0900

    Title: 날짜 변경하여 출력, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260103/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.next().split("\\.");
+        System.out.printf("%s-%s-%s", strArr[1], strArr[2], strArr[0]);
+    }
+}
